aa7bb8e861f6a1e2a6bdbe1683c929ce9e95137a
KSN99 <53466034+KSN99@users.noreply.github.com>
2022-01-15 23:33:20 +0900

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+# def chat(name1, name2,age):
+#     print("%s: How old r u?"%name1)
+#     print("%s: Im %d" %(name2,age))
+# chat("a","b",10)#parameter 인자 
+
+def dsum(a,b):
+    result = a+b
+    return result
+
+d= dsum(1,2)
+print(d)
