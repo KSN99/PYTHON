5037565a30f5e82aa41dcce87edd83ed7f6a9eb5
KSN99 <53466034+KSN99@users.noreply.github.com>
2022-01-15 20:45:51 +0900

    python

---
+++ 3.py
@@ -0,0 +1,5 @@
+a="hobby"
+a.count('b')
+a.count()
+
+print(a)
---
+++ main.py
@@ -0,0 +1,19 @@
+x="hello"
+y="bye"
+
+z="""
+Hello 
+MY name is kim . 
+"""
+# """""" 따옴표 세 개는 띄어쓰기 가능 
+print(x)
+print(y)
+print(z)
+
+a=4 
+b="4"
+
+print(str(a)+b)
+
+# Boolean
+
