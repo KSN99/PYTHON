5d2f25bb328c6b5c200e663c7cbee15dd08d2ee4
KSN99 <53466034+KSN99@users.noreply.github.com>
2022-01-15 21:25:17 +0900

    조건문 python

---
+++ 조건문.py
@@ -0,0 +1,20 @@
+# if 2>1:
+#     print("hello")
+
+# if not 1>2:
+#     print("Hi")
+
+# if 1>2 and 2>1:
+#     print(" Aneyoend")
+
+# if 0>0 or 2>1:
+#     print("fdsafsd")
+
+x=3 
+
+if x>3:
+    print("Fuck")
+elif x==3:
+    print("Love you")
+else:
+    print("you")
