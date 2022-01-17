c71e452adca59231c737812064989a6125d4178f
KSN99 <53466034+KSN99@users.noreply.github.com>
2022-01-17 15:32:47 +0900

    tuple

---
+++ tuple.py
@@ -0,0 +1,21 @@
+x= tuple()
+y=()
+
+print(x)
+
+x=(1,2,3)
+y=('a','b','c')
+z=(1,"hello","there")
+
+print(x)
+print(y)
+print(z)
+print(x+y)
+print('a' in y )
+print(z.index(1))
+
+# Difference between Tuple and List
+# Assignment: Tuple안의 값을 업데이트 하는 것 
+#x[0] = 10  # 불가능 ! 
+# List ( Mutable 가변) /Tuple (immutable불변)
+
