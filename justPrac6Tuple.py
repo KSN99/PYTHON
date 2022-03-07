79dcb1a3e523dd15c8bd1ee9e9dab4d555b6e6c8
KSN <v999vvv@naver.com>
2022-03-07 16:40:34 +0900

    Tuple

---
+++ justPrac6Tuple.py
@@ -0,0 +1,16 @@
+#Tuple 
+# 속도가 리스트보다 빠름
+
+menu = ("A","B")
+print(menu[0])
+print(menu[1])
+
+#tuple 은 add 불가능 값 추가 변경 x 
+
+name = "김종국"
+age = 20
+hobby = "coding"
+print(name, age,hobby)
+
+(name,age,hobby) = ("김종국",20,"coding")
+print(name,age,hobby)s
