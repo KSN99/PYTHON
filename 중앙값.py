ae38374ab8d16b7a99dd660154602dc01df52324
KSN <v999vvv@naver.com>
2022-05-15 22:18:01 +0900

    med

---
+++ 중앙값.py
@@ -0,0 +1,10 @@
+# 세 정수를 입력받아 중앙값 구하기 1
+
+def med3(a,b,c):
+    if (b >=a and c<=a) or (b <= a and c>=a):
+        return a
+
+    elif ( a>b and c<b) or (a<b and c>b):
+        return b
+    
+    return c 
