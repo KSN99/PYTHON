7a163d658313c79ccfb000998625c3de72d10f85
KSN <v999vvv@naver.com>
2022-05-18 20:31:23 +0900

    baekjoon

--- just.py
+++ just.py
@@ -1,14 +1,14 @@
-A,B= map(int,input().split())
-C=int(input())
+# random , 주사위 3개ㅡ> 변수 설정
+# 나온 눈의 수로 상금 계산. 
+a, b, c = map(int, input().split())
 
-A +=C//60
-B +=C%60
-
-if B >=60:
-    A+=1
-    B-=60
-
-if A>=24:
-    A-=24
-
-print(A,B)
+if a == b == c:
+    print(10000+a*1000)
+elif a == b:
+    print(1000+a*100)
+elif a == c:
+    print(1000+a*100)
+elif b == c:
+    print(1000+b*100)
+else:
+    print(100 * max(a,b,c))
