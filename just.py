7c3a2f8c892bba4dacd02005208e8180b1cf3e5f
KSN <v999vvv@naver.com>
2022-05-31 20:45:30 +0900

    prac

--- just.py
+++ just.py
@@ -1,14 +1,4 @@
-# random , 주사위 3개ㅡ> 변수 설정
-# 나온 눈의 수로 상금 계산. 
-a, b, c = map(int, input().split())
+n = int(input())
 
-if a == b == c:
-    print(10000+a*1000)
-elif a == b:
-    print(1000+a*100)
-elif a == c:
-    print(1000+a*100)
-elif b == c:
-    print(1000+b*100)
-else:
-    print(100 * max(a,b,c))
+for i in range(1,10):  # 1~9
+    print(n, '*', i, '=', n*i)
