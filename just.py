2b6c292e76053ff12af7089ca8cc7a5e74f2af06
KSN <v999vvv@naver.com>
2022-06-15 23:07:02 +0900

    10871 baekjoon

--- just.py
+++ just.py
@@ -1,4 +1,5 @@
-n=int(input())
-
-for i in range(1,n+1):
-    print(" "*(n-i) + "*"*i)
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+for i in range(N):
+    if A[i] < X:
+        print(A[i], end=" ")
