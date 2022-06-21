420108239a79a94f2dcc168420ce27fbc432fd31
KSN <v999vvv@naver.com>
2022-06-21 20:45:42 +0900

    백준 10952

--- just.py
+++ just.py
@@ -1,5 +1,5 @@
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
-for i in range(N):
-    if A[i] < X:
-        print(A[i], end=" ")
+while True:
+    A,B=map(int,input().split())
+    if A==0 or B==0:
+        break
+    print(A+B)
