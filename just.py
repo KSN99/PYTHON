8f87b9df73db91927d11902692bb205171318ced
KSN <v999vvv@naver.com>
2022-07-05 18:40:08 +0900

    exception

--- just.py
+++ just.py
@@ -6,3 +6,10 @@ for i in range(9):
 
 print(max(num_list))
 print(num_list.index(max(num_iist))+1)
+
+num_list = []
+for i in range(9):
+    num_list.append(int(input()))
+    
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
