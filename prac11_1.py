b2fcd54ee830e00d370653dc083b2a11e6713fb3
KSN <v999vvv@naver.com>
2022-07-05 19:57:42 +0900

    module import

---
+++ prac11_1.py
@@ -0,0 +1,15 @@
+# import prac11
+
+# prac11.price(3) # 3명이서 영화보러 갔을 때 가격. 
+# prac11.price_morning(4)
+# prac11.price_soldier(5)
+
+# import prac11 as mv  # 모듈명이 길 때 별명을 통해 줄여서 사용가능. 
+# mv.price(3)
+# mv.price_morning(4) 
+
+from prac11 import*
+#from random import*
+
+price(3)
+price_soldier(5)
