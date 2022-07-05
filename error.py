0e14a0a5deeb00816c6b10d3d0ce19e6e1655ad7
KSN <v999vvv@naver.com>
2022-07-05 19:44:46 +0900

    error

--- error.py
+++ error.py
@@ -1,10 +1,23 @@
+class BigNumberError(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+    
+    def __str__(self):
+        return self.msg
+
 try:
     print("한 자리 숫자 나누기 전용 계산기")
     num1=int(input("first num:"))
     num2=int(input("second num:"))
     if num1>=10 or num2>=10:
-        raise ValueError
+        raise BigNumberError
     print("{0} / {1} = {2}".format(num1,num2,int(num1/num2)))
 
 except ValueError:
     print("wrong number 한 자리 숫자만 가능.")
+
+except BigNumberError:
+    print("Wrong number . ")
+
+finally:
+    print("Thank you for using it.")
